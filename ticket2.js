bad889dee29341d956983c98cf0d91025536e6fa
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2024-12-02 14:14:08 +0000

    mvp 2 complete, gbp to dollars

---
+++ ticket2.js
@@ -0,0 +1,11 @@
+//MVP 2
+  
+//Write another function `convertGBPtoUSD` 
+//that takes in a specified amount in £,
+//and converts it to $ using a given exchange rate. 
+//It should behave in a very similar way to the previous function.
+
+function convertGBPtoUSD(pounds) {
+ return dollars = pounds * 1.27;
+
+}
