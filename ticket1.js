a4c7ebe92595a2003d5ecfa9209ce6d777de2604
Timothy Li <timothy93li@gmail.com>
2024-12-02 14:02:59 +0000

    mvp 1 done, gbp eur conversion function

---
+++ ticket1.js
@@ -0,0 +1,8 @@
+// Write a JavaScript function `convertGBPtoEUR`
+// that takes in a specified amount in £,
+// and converts it to € using a given exchange rate.
+// The function should return the converted amount.
+
+function convertGBPtoEUR(pounds) {
+  return euro = pounds * 1.21;
+}
